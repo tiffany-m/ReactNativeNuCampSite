2263ed72a3cfc1b37293ff3c305ad66f3f6bd52a
Tiffany <tiffany_mollica@yahoo.com>
2020-09-28 15:50:12 -0400

    Workshop Week 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -27,6 +27,8 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -53,6 +55,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
